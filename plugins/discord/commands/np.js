ebd46ade26c4e16f44104298d1d3ebbf55772783
dogr95 <rayquasa181@gmail.com>
2020-10-14 10:37:23 +0200

    small changes to now playing

--- plugins/discord/commands/np.js
+++ plugins/discord/commands/np.js
@@ -20,7 +20,7 @@ module.exports = function(client, message, args) {
     streamtimeformat = "0:00"
   }
   let queue = "";
-  for(let i = 0; i < client.mediaPlayer.queue.length; i++) {
+  for(let i = 0; i < client.mediaPlayer.queue.length && i < 5; i++) {
     queue += `[${i}]` + " " +client.mediaPlayer.queue[i] + "\n"
   }
   let embed = new discord.MessageEmbed()
@@ -29,7 +29,7 @@ module.exports = function(client, message, args) {
   .setURL('https://zap-hosting.com/de/shop/donation/b46e5e7b07106dad59febaf3b66fd5e5/')
   .setAuthor('HahnrichJS', 'https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/cb/cb9a41873f2065b8010afa7584803d283dd7e6ad_full.jpg', 'https://alleshusos.de')
   .addField(`${client.mediaPlayer.now_playing !== '' ? client.mediaPlayer.now_playing : "not playing anything"}`, `${streamtimeformat}/${typeof client.mediaPlayer.currentLength !== "undefined" ? client.mediaPlayer.currentLength : "0:00"}`)
-  .setFooter(`Queue:\n${queue.length > 1500 ? queue.susbstring(0, 1500) : queue}`)
+  .setFooter(`Queue[${client.mediaPlayer.queue.length}]:\n${queue}`)
   message.reply(embed)
   //message.reply(client.mediaPlayer.now_playing + "\nQueue: \n" + client.mediaPlayer.queue.join('\n'))
 }
