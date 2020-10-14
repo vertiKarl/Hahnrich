8a74c085a349cc9413c17b4d6fcb6d3fd18562f5
dogr95 <rayquasa181@gmail.com>
2020-10-14 22:10:54 +0200

    ~

--- plugins/twitch/commands/discord.js
+++ plugins/twitch/commands/discord.js
@@ -1,3 +1,2 @@
 module.exports = function(twitch, chat, pubAPI, channel, user, args) {
-  chat.action(channel, `Ahoy @${user[display-name]}, https://discord.gg/qew4dba`)
-}
+  chat.action(channel, `Ahoy @${user}, https://discord.gg/qew4dba`)
